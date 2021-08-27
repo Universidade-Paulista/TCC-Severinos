d452486538b556fde76086187428703e80e3a674
HarleyLiberato <63554930+HarleyLiberato@users.noreply.github.com>
2021-08-27 20:59:06 -0300

    Update Login.dart

--- lib/telas/Login.dart
+++ lib/telas/Login.dart
@@ -1,6 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class LoginL extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 300,
+              height: 300,
+              child: Image.asset('assets/Logos/LogoRedonda.png'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  labelText: "E-mail",
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
@@ -14,17 +52,56 @@ class _LoginState extends State<Login> {
         title: Image.asset(
           'assets/Logos/LogoEscrita.png',
           height: 225,
-          width: 600,
+          width: 600,R
           alignment: Alignment.centerLeft,
         ),
       ),*/
       body: Container(
-        child: Column(
-          children: [
-            logo(),
-            campos(),
-            Align(
-              alignment: Alignment.center,
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 300,
+              height: 300,
+              child: Image.asset('assets/Logos/LogoRedonda.png'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  labelText: "E-mail",
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  )),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  )),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+              autofocus: true,
             )
           ],
         ),
@@ -36,14 +113,20 @@ class _LoginState extends State<Login> {
     return Container(
       width: 300,
       height: 300,
-      alignment: Alignment.center,
       child: Image.asset('assets/Logos/LogoRedonda.png'),
     );
   }
 
-  Container campos(){
+  Container campos() {
     return Container(
-      child: Column(children: [TextField(decoration: InputDecoration(hintText: "E-mail", labelText: "E-mail"),)],),
-    )
+      child: Column(
+        children: [
+          TextField(
+            decoration:
+                InputDecoration(hintText: "E-mail", labelText: "E-mail"),
+          )
+        ],
+      ),
+    );
   }
 }
