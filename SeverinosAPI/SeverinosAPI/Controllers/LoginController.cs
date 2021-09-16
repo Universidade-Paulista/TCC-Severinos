032f7f147a260922e33883dcadfe450932229ae9
HarleyLiberato <63554930+HarleyLiberato@users.noreply.github.com>
2021-09-16 19:55:51 -0300

    api

---
+++ SeverinosAPI/SeverinosAPI/Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SeverinosAPI.Models;
+using TCCteste;
+
+namespace Severinoslogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+
+        public IActionResult RequestLogin([FromBody] Usuarios request)
+        {
+
+            DBModel.GetConexao();
+
+            var pessoa = DBModel.GetReader("select nome from tb_pessoa ");
+
+            pessoa.Read();
+            var tes = pessoa["nome"].ToString();            
+
+            if (pessoa["nome"].ToString() == "teste")
+            {
+                return Ok(new string[] {"Usuario Encontrado"});
+
+                //var claims = new[]
+                //{
+                //    new Claim(ClaimTypes.Name, request.Nome)
+                //};
+
+                //var key = new SymmetricSecurityKey(
+                //    Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+
+                //var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                //var token = new JwtSecurityToken(
+                //    issuer: "Severinos",
+                //    audience: "Severinos",
+                //    claims: claims,
+                //    expires: DateTime.Now.AddMonths(30),
+                //    signingCredentials: creds);
+
+                //return Ok(new
+                //{
+                //    token = new JwtSecurityTokenHandler().WriteToken(token)
+                //});
+            }
+            return BadRequest("Usuario não encontrado");
+        }
+    }
+}
---
+++ SeverinosAPI/SeverinosAPI/DBModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace TCCteste
+{
+    public class DBModel
+    {
+        public static Npgsql.NpgsqlConnection GetConexao()
+        {
+            NpgsqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = DBModel.GetConnectionString();
+
+            try
+            {
+                conn = new NpgsqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+                return conn;
+            }
+            catch (Npgsql.NpgsqlException Ex)
+            {
+                throw new Exception(Ex.Message);
+            }
+        }
+
+        private static String GetConnectionString()
+        {
+            string sRetorno = "";
+
+            sRetorno = "User ID=eyzsiosffqeroz;" +
+                       "Password=75f2eec3886a903f63b330acae42b36d5eff5e6381c9232333bdc00e1639b200;" +
+                       "Host=ec2-35-153-114-74.compute-1.amazonaws.com;" +
+                       "Port=5432;" +
+                       "Database=d13nbustk8eoiv;" +
+                       "Pooling=true;" +
+                       "SSL Mode=Require;" +
+                       "TrustServerCertificate=True;";
+
+            return sRetorno;
+        }
+
+        public static Npgsql.NpgsqlDataReader GetReader(string sSql)
+        {
+            NpgsqlDataReader drRetorno;
+            NpgsqlCommand oCmd = new NpgsqlCommand();
+            oCmd.Connection = DBModel.GetConexao();
+            oCmd.CommandText = sSql;
+
+            drRetorno = oCmd.ExecuteReader();
+
+            return drRetorno;
+        }
+
+        //public static int RunSqlNonQuery(string sSql)
+        //{
+        //    MySqlCommand oCmd = new MySqlCommand();
+        //    oCmd.Connection = DBModel.GetConexao();
+        //    oCmd.CommandText = sSql;
+
+        //    int iRetorno = oCmd.ExecuteNonQuery();
+
+        //    DBModel.CleanUPDBCmd(ref oCmd);
+
+        //    return iRetorno;
+        //}
+
+        //public static void CleanUPDBReader(ref MySqlDataReader oDr)
+        //{
+        //    if (oDr != null)
+        //    {
+        //        oDr.Dispose();
+        //    }
+        //}
+
+        //public static void CleanUPDBCmd(ref MySqlCommand oCmd)
+        //{
+        //    if (oCmd != null)
+        //    {
+        //        if (oCmd.Connection.State == ConnectionState.Open)
+        //        {
+        //            oCmd.Connection.Clone();
+        //        }
+        //        oCmd.Connection.Dispose();
+
+        //        oCmd.Dispose();
+        //    }
+        //}
+    }
+}
